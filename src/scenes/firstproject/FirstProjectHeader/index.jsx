0b0906e9d257848752cad7baff399cdb964c76fe
kssc90 <51017284+kssc90@users.noreply.github.com>
2022-12-20 02:07:46 +0530

    Small Change

--- src/scenes/firstproject/FirstProjectHeader/index.jsx
+++ src/scenes/firstproject/FirstProjectHeader/index.jsx
@@ -3,16 +3,18 @@ import React from "react";
 
 export const FirstProjectHeader = () => {
   return (
-    <Box padding="20px">
-      <Typography>
-        Keyword Explorer &gt; <b>Keyword Overview</b>
-      </Typography>
-      <Typography variant="h6">
-        <b>Keyword:</b> shopping in barcelona
-      </Typography>
-      <Typography variant="p2">Database: United States </Typography>
-    </Box>
-    <Box></Box>
+    <>
+      <Box padding="20px">
+        <Typography>
+          Keyword Explorer &gt; <b>Keyword Overview</b>
+        </Typography>
+        <Typography variant="h6">
+          <b>Keyword:</b> shopping in barcelona
+        </Typography>
+        <Typography variant="p2">Database: United States </Typography>
+      </Box>
+      <Box></Box>
+    </>
   );
 };
 
